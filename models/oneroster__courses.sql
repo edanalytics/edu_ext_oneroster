a306a97a3243ba6e2b04dac00a3462ff4e2cca7a
keenzarate213 <kzarate@edanalytics.org>
2023-10-18 15:30:38 -0500

    add crs_offering join to grab k_school

--- models/oneroster__courses.sql
+++ models/oneroster__courses.sql
@@ -1,5 +1,8 @@
 with stg_courses as (
     select * from {{ ref('stg_ef3__courses')}}
+),
+stg_course_offering as (
+    select * from {{ ref('stg_ef3__course_offerings')}}
 )
 select 
     crs.k_course as sourcedId, 
@@ -8,7 +11,9 @@ select
     crs.course_title as title, 
     crs.course_code  as courseCode, 
     -- v_offered_grade_levels as grades [currently an array need to flatten out] -- also not required field
-    concat(crs.tenant_code, crs.ed_org_id) as orgSourcedId
+    crs_offering.k_school as orgSourcedId
     -- subjects   
     -- subjectCodes
 from stg_courses crs
+join stg_course_offering crs_offering on crs.k_course = crs_offering.k_course
+
