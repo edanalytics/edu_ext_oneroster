19d00431517b25343eb39ae34994368cbe198fba
ejoranlien <ejoranlien@edanalytics.org>
2024-02-06 18:01:05 -0600

    add default var value

--- models/oneroster_1_1/or1_1__department_helper.sql
+++ models/oneroster_1_1/or1_1__department_helper.sql
@@ -10,7 +10,7 @@ select
     ed_org_id,
     -- fill NULL values, strip commas (for CSV formatting)
     coalesce(
-        replace({{ var('oneroster:course_dept_column') }}, ','), 
+        replace({{ var('oneroster:course_dept_column', '') }}, ','), 
         'Unknown') as department_name
 from {{ ref('dim_course') }}
 where school_year = {{ var('oneroster:active_school_year')}}
