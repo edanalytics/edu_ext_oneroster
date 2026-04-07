a7e0d5f3047621ed1c66fb1f136ba5fd36bb39ff
Greg Fitzgerald <gfitzgerald>
2026-04-07 14:46:19 -0400

    Drop inactive enrollments and assignments from enrollments model.

--- models/oneroster_1_1/or1_1__enrollments.sql
+++ models/oneroster_1_1/or1_1__enrollments.sql
@@ -21,6 +21,15 @@ xwalk_classroom_positions as (
     select * from {{ ref('xwalk_oneroster_classroom_positions')}}
 ),
 
+active_staff as (
+    select * from stg_staff_section_associations
+    where begin_date <= current_date()
+        and (
+            end_date >= current_date()
+            or end_date is null
+        )
+),
+
 staff_enrollments_formatted as (
     select
         {{ gen_sourced_id('staff_enr') }} as "sourcedId",
@@ -35,9 +44,18 @@ staff_enrollments_formatted as (
         end_date as "endDate",
         {{ gen_natural_key('staff_enr') }} as "metadata.edu.natural_key",
         ssa.tenant_code
-    from stg_staff_section_associations ssa
+    from active_staff as ssa
     left join xwalk_classroom_positions xwalk
         on ssa.classroom_position = xwalk.classroom_position
+),
+
+active_students as (
+    select * from stg_student_section_associations
+    where begin_date <= current_date()
+        and (
+            end_date >= current_date()
+            or end_date is null
+        )
 ),
 
 student_enrollments_formatted as (
@@ -53,8 +71,8 @@ student_enrollments_formatted as (
         begin_date as "beginDate",
         end_date as "endDate",
         {{ gen_natural_key('stu_enr') }} as "metadata.edu.natural_key",
-        ssa.tenant_code
-    from stg_student_section_associations ssa
+        tenant_code
+    from active_students
 )
 select * from staff_enrollments_formatted
 union all
